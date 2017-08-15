eab71879dd5b167652ae4d891de630958cb752a3
Masya120 <maxk893@gmail.com>
2017-08-14 19:43:04 -0700

    Added option for user to get def of suggested word

--- app1.py
+++ app1.py
@@ -3,18 +3,25 @@ from difflib import get_close_matches
 
 data = json.load(open("data.json", "r"))
 
-word = input("Type your word: ")
+dataKeys = data.keys()
 
+word = input("Type your word: ")
 
 def getDef(w):
 
     w = w.lower()
     if w in data:
         return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+    elif len(get_close_matches(w, dataKeys)) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, dataKeys)[0])
+        if yn == "Y" or yn == "y":
+            return data[get_close_matches(w, dataKeys)[0]]
+        elif yn == "N" or yn == "n":
+            return("Word not found.")
+        else:
+            return("We didn't understand your entry")
     else:
-        return("Word not found. Did you mean to type this word? " + get_close_matches(w, data.keys(), n=3, cutoff=0.8)[0])
+        return("Word not found.")
 
 
 print(getDef(word))
