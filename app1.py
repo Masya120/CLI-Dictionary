e7d0b623c3fb6eeb17aec27131d4453763264305
Masya120 <maxk893@gmail.com>
2017-08-14 18:50:47 -0700

    Fixed bug in similar word check

--- app1.py
+++ app1.py
@@ -11,7 +11,7 @@ def getDef(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif len(get_close_matches(w, data.keys())):
+    elif len(get_close_matches(w, data.keys())) > 0:
         return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
     else:
         return("Word not found. Did you mean to type this word? " + get_close_matches(w, data.keys(), n=3, cutoff=0.8)[0])
