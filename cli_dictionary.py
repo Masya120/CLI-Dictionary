c71d72a31c50eef60e64f9fa4482f8654bb87005
Max <maxk893@gmail.com>
2018-03-10 12:15:10 -0800

    Moved getting close matches to word into separate function.

--- cli_dictionary.py
+++ cli_dictionary.py
@@ -12,16 +12,18 @@ def get_def(word):
     word = word.lower()
     if word in data:
         return data[word]
-    elif len(get_close_matches(word, dataKeys)) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, dataKeys)[0])
-        if yn == "Y" or yn == "y":
-            return data[get_close_matches(word, dataKeys)[0]]
-        elif yn == "N" or yn == "n":
-            return("Word not found.")
-        else:
-            return("We didn't understand your entry")
     else:
-        return("Word not found.")
+        return get_matches(word)
+
+def get_matches(word):
+    if len(get_close_matches(word, dataKeys)) > 0:
+        user_response = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, dataKeys)[0])
+        if user_response == "Y" or user_response == "y":
+            return data[get_close_matches(word, dataKeys)[0]]
+        elif user_response == "N" or user_response == "n":
+            return "Word not found."
+        else:
+            return "We didn't understand your entry"
 
 def print_def(output):
     if type(output) == list:
